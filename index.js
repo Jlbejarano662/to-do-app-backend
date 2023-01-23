3538b7e96bf460cb32bb4cbb3e3ac6375b3e7092
Jlbejarano662 <jasmin.1207@hotmail.com>
2023-01-23 14:34:19 -0500

    deploy

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const PORT = 3030;
+require('dotenv').config({ path: '.env'});
+
+
 const app = express();
+const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 3030
 
 const todoRoutes = require("./routes/todoRoutes");
 const connectionOptions = {
@@ -16,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://127.0.0.1/todolist", connectionOptions)
+  .connect(MONGODB_URI, connectionOptions)
   .then(() => console.log("Connected successfully"))
   .catch((err) => console.error(err));
 
